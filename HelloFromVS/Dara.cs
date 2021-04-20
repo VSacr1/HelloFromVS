e477d061dadb167d828147add39a96b5227f4b75
Dara Oladapo <daraoladapo@outlook.com>
2021-04-20 10:37:45 +0100

    Add project files.

---
+++ HelloFromVS/Dara.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloFromVS
+{
+    class Dara
+    {
+        static void Print()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ HelloFromVS/Program.cs
@@ -0,0 +1,228 @@
+﻿
+using System;
+using System.Linq;
+
+namespace HelloFromVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("What is your name? ");
+            //var name = Console.ReadLine();
+            //var date = DateTime.Now;
+            //Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+
+            //declare
+            //int myInt;
+            //assigning
+            //myInt = 12334568;
+            //instantiation
+            //long myLong = 123343454534534;
+            //float myFloat = 2.2564546F;
+            //double myDouble = 12.245645465;
+            //bool myBool = true;
+            //bool myOtherBool = false;
+            //char myChar = 'J';
+            //string myString = "a bit longer and contains more character";
+
+            //var myVar = 1232134345;
+
+            //string firstName = "Dara";
+            //int age = 256;
+            //Console.WriteLine(firstName + " is " + age + " year old.");
+
+
+            //Console.WriteLine(34 + "+" + 56 + "=" + (34 + 56));
+
+            //Console.WriteLine("Enter username: ");
+            //var userName = Console.ReadLine();
+            //Console.WriteLine("User name is : " + userName);
+            //Console.WriteLine("Enter age: ");
+            //int age = Console.ReadLine();
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Age is " + age);
+            //Console.WriteLine(1);
+            //Console.WriteLine(2);
+            //Console.WriteLine(3);
+            //Console.WriteLine(4);
+            //Console.WriteLine(5);
+            //go to 1000000
+            //multiples of 5,10,15,20,.....1000
+            //for (int i = 5; i <= 1000; i += 5)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //string[] myClass = { "Ben", "Dara", "Victoria" };
+            //foreach (var member in myClass)
+            //{
+            //    Console.WriteLine(member);
+            //}
+            //while it's cold, turn on the heater.
+            //while it's hot, turn on the fan.
+            //Console.WriteLine("While Loop");
+            //int i = 100;
+            //while (i > 10 && i <= 50)
+            //{
+            //    Console.WriteLine(i);
+            //    i += 5;
+            //}
+            //Console.WriteLine("Do-While Loop");
+            //int j = 100;
+            //do
+            //{
+            //    Console.WriteLine(j);
+            //    j += 5;
+            //} while (j > 10 && j <= 50);
+            //string greeting = "Hello Folks|Hi People|Hi Y'all";
+            //Console.WriteLine(greeting);
+            //string[] greetingSplitCollection = greeting.Split('|');
+            //foreach (var greetingItem in greetingSplitCollection)
+            //{
+            //    Console.WriteLine(greetingItem);
+            //}
+            //string greeting = "Hello World, it's a beautiful day.";
+            //Console.WriteLine(greeting.Contains("beautiful"));
+            //Console.WriteLine(greeting);
+            //string replacedGreeting = greeting.Replace("beautiful", "good");
+            //Console.WriteLine(greeting.Replace("beautiful", "good"));
+            //Console.WriteLine(greeting);
+            // This is Dara's Visual Studio
+            //Console.WriteLine("This is Dara's Visual Studio");
+            //Console.WriteLine("Dara said \"This is Dara's Visual Studio\"");
+            //Console.WriteLine('\'');
+            //Console.WriteLine("This is a new");
+            //Console.WriteLine("line");
+            //Console.WriteLine("This is a new\nline");
+            //Console.WriteLine("Name\tEmail Address\tPhone Number");
+            //Console.WriteLine("Dara\tdoladapo@qa.com\t012345678");
+            //Console.WriteLine("Daro\tdooola@qa.com\t012345678");
+
+            //string firstName = "Dara", lastName = "Oladapo";
+            //string fullName = firstName + " " + lastName;
+            //Console.WriteLine(fullName);
+            //Console.WriteLine("{0} {1}", firstName, lastName);
+            //Console.WriteLine($"{firstName} {lastName}");
+
+            //string name = "Dara";
+            //int firstNumber = 2, secondNumber = 52;
+            //Console.WriteLine(name + " " + firstNumber);
+            //Console.WriteLine(name + " " + (firstNumber + secondNumber));
+            //Console.WriteLine($"{name} {firstNumber + secondNumber}");
+
+            //if a user is 18+
+            //comparison in # - <:less than, >: greater than, <=: less than or equal to, >=:greater than or equal to, ==: equal to, !=: not equal to
+            // ||: or &&: and
+
+            //var age = int.Parse(Console.ReadLine());
+            //if (age <= 18)
+            //{
+            //    Console.WriteLine("Sorry, not old enough.");
+            //}
+            //else if (age >= 65)
+            //{
+            //    Console.WriteLine("Too old to join, you should be retired.");
+            //}
+            //else if (age < 0 || age > 150)
+            //{
+            //    Console.WriteLine("Age value is invalid");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Welcome to workforce.");
+            //}
+
+            //if (true && true && true) { }
+            //if (true)
+            //{
+            //    if (true)
+            //    {
+            //        if (true)
+            //        {
+            //            if (true)
+            //            {
+
+            //            }
+            //        }
+            //    }
+            //}
+
+            //grades: A, B, C, D, E, F
+            /*
+             * A: 70
+             * B: 60
+             * C: 50
+             * D: 45
+             * E: 40
+             * F: 10
+             */
+            //int score = int.Parse(Console.ReadLine());
+            //switch (score)
+            //{
+            //    case 70:
+            //        Console.WriteLine("A");
+            //        break;
+            //    case 60:
+            //        Console.WriteLine("B");
+            //        break;
+            //    case 50:
+            //        Console.WriteLine("C");
+            //        break;
+            //    case 45:
+            //        Console.WriteLine("D");
+            //        break;
+            //    case 40:
+            //        Console.WriteLine("E");
+            //        break;
+            //default:
+            //    Console.WriteLine("F");
+            //    break;
+
+            ////}
+
+            //int addition = 2 + 2;//4
+            //double multiplication = 2 * 3.0;//6.0
+
+            //int remainderDiv = 5 % 3;//2
+
+            //double calculation = 2 * 3 + 3 + 4 - 7 * 3 / 2.0 + (3 * 2);
+            //Console.WriteLine(calculation);
+            //squareroot of 25
+            //double sqrt = Math.Sqrt(52);
+            //Console.WriteLine(sqrt);
+
+            //area of circle = Pi * r*r
+            //double radius = 3.5;
+            //double area = Math.PI * radius * radius;
+
+            //Math.Pow(81, 1/3);
+            //string[] cars = { "Ford", "Vauxhaul", "Toyota", "Nissan" };
+            //string[] otherCars;
+            //foreach (var car in cars)
+            //{
+            //    Console.WriteLine(car);
+            //}
+            //otherCars = new string[4];
+            //otherCars[0] = "Mercedez";
+            //otherCars[1] = "Mazda";
+            //otherCars[2] = "Tesla";
+            //otherCars[3] = "Volvo";
+            //Array.Sort(otherCars);
+            //foreach (var car in otherCars)
+            //{
+            //    Console.WriteLine(car);
+            //}
+            //otherCars = new string[5];
+            //otherCars[4] = "Skoda";
+            //Console.WriteLine(otherCars[0]);
+
+            int[] myNumbers = { 5, 1, 8, 9 };
+            //non-helped way - bubble sorting.
+            int maxValue = myNumbers.Max();
+            Console.WriteLine(maxValue);
+            int minValue = myNumbers.Min();
+            Console.WriteLine(minValue);
+        }
+    }
+}
