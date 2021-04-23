fac9d94b8f181af981a33efb81845ad215d1352b
Dara Oladapo <daraoladapo@outlook.com>
2021-04-23 13:25:07 +0100

    files

---
+++ Exercises/DataFiles/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+namespace DataFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = @"C:\Dev\test2.txt.bac";
+            //var doesFileExist = File.Exists(filePath);
+            //Console.WriteLine($"{filePath} exists?: {doesFileExist}"); ;
+            File.Delete(filePath);
+        }
+    }
+}
